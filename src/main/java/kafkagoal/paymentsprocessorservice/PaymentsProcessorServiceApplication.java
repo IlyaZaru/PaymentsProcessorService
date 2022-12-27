f0c9b264b8bc3c2ed03d2ab3cc7c735136518b61
izarubin <izarubin@neoflex.ru>
2022-12-27 14:16:37 +0300

    init commit

---
+++ src/main/java/kafkagoal/paymentsprocessorservice/PaymentsProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package kafkagoal.paymentsprocessorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsProcessorServiceApplication.class, args);
+	}
+
+}
